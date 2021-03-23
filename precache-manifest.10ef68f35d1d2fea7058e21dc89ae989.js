97380219cad2ae429bbd79711b50f268d4bb38a0
Владислав Сандык <nubia@MacBook-Pro-Vladislav.local>
2021-03-23 14:17:00 +0200

    Updates

---
+++ precache-manifest.10ef68f35d1d2fea7058e21dc89ae989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405df6de01b76f124e7d747d17c7b086",
+    "url": "./index.html"
+  },
+  {
+    "revision": "91f4e2a226ae5326d6f4",
+    "url": "./static/css/main.438033a4.chunk.css"
+  },
+  {
+    "revision": "c07500df0ccde94f4968",
+    "url": "./static/js/2.f3cc3ca8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f3cc3ca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f4e2a226ae5326d6f4",
+    "url": "./static/js/main.329b7fc3.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
